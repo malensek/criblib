248bdc32ad539b9f8a5038ca983320836d88db44
Matthew Malensek <matt@malensek.net>
2013-11-19 16:57:24 -0700

    Add fancy suit characters

--- src/jcrib/Suit.java
+++ src/jcrib/Suit.java
@@ -6,32 +6,39 @@ public enum Suit {
     Diamond,
     Club;
 
+    private static boolean fancyChars = false;
+
     public static Suit fromString(String s) {
         s = s.toLowerCase();
         switch (s) {
-            case "s":
-                return Spade;
-            case "h":
-                return Heart;
-            case "d":
-                return Diamond;
-            case "c":
-                return Club;
+            case "s": return Spade;
+            case "h": return Heart;
+            case "d": return Diamond;
+            case "c": return Club;
         }
         return null;
     }
 
+    public static void enableFancyChars() {
+        fancyChars = true;
+    }
+
     @Override
     public String toString() {
-        switch (this) {
-            case Spade:
-                return "s";
-            case Heart:
-                return "h";
-            case Diamond:
-                return "d";
-            case Club:
-                return "c";
+        if (fancyChars) {
+            switch (this) {
+                case Spade: return "♠";
+                case Heart: return "♥";
+                case Diamond: return "♦";
+                case Club: return "♣";
+            }
+        } else {
+            switch (this) {
+                case Spade: return "s";
+                case Heart: return "h";
+                case Diamond: return "d";
+                case Club: return "c";
+            }
         }
         return null;
     }
