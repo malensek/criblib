6c57d0bced89d1f770867e30622374a535d77d3e
Matthew Malensek <matt@malensek.net>
2013-11-19 17:04:56 -0700

    Add IllegalPlayException for invalid moves

---
+++ src/jcrib/IllegalPlayException.java
@@ -0,0 +1,14 @@
+package jcrib;
+
+public class IllegalPlayException extends Exception {
+
+    static final long serialVersionUID = 6999197812135492022L;
+
+    public IllegalPlayException() {
+        super();
+    }
+
+    public IllegalPlayException(String s) {
+        super(s);
+    }
+}
