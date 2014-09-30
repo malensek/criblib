cf3d4e1aba4eaa28c1ae04ea1003e006447dfdea
Matthew Malensek <matt@malensek.net>
2014-09-29 21:18:49 -0600

    Implement hashCode()

--- src/jcrib/cards/Card.java
+++ src/jcrib/cards/Card.java
@@ -87,26 +87,37 @@ public class Card implements Comparable<Card> {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((face == null) ? 0 : face.hashCode());
+        result = prime * result + ((suit == null) ? 0 : suit.hashCode());
+        result = prime * result + value;
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
-
         if (obj == null) {
             return false;
         }
-
         if (getClass() != obj.getClass()) {
             return false;
         }
-
         Card other = (Card) obj;
-
-        if (this.suit != other.suit) {
+        if (face != other.face) {
             return false;
         }
-
-        return compareTo((Card) obj) == 0;
+        if (suit != other.suit) {
+            return false;
+        }
+        if (value != other.value) {
+            return false;
+        }
+        return true;
     }
 
     @Override
